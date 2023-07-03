95968721c0bad6bee15cdf1dc0a160a3bf74849a
Antonio Sitoe <antonio.sitoe@void.co.mz>
2023-07-03 22:00:09 +0200

    componentes na web e autenticacao server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,16 @@
+import 'dotenv/config';
 import fastify = require('fastify');
 import cors from '@fastify/cors';
+import jwt from '@fastify/jwt';
 import { memoriesRoutes } from './routes/memories';
 
 const app = fastify();
 app.register(memoriesRoutes);
 app.register(cors, {
   origin: true, // todas url de frontend podem acessar,
+});
+app.register(jwt, {
+  secret: 'spacetime-XXP', // maneira de diferenciar os jwt desse backend dos outros backends
 });
 
 app
